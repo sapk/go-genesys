872bf919204e84961207a8384325466ff77cb552
Antoine GIRARD <sapk@users.noreply.github.com>
2020-03-10 15:37:48 +0100

    Cleaning-up

--- examples/get-persons/get-persons.go
+++ examples/get-persons/get-persons.go
@@ -33,17 +33,6 @@ func main() {
 	}
 	for _, p := range personList {
 		fmt.Printf("%s %s\n", p.Firstname, p.Lastname)
-		//fmt.Printf("bytes: %#v\n", toByte(p.Firstname))
-		//fmt.Printf("test1: %#v\n", string(toByte(p.Firstname)))
-		//fmt.Printf("test2: %#v\n", bytes.NewBuffer(toByte(p.Firstname)).String())
 	}
 
-	//tBytes := []byte{0x5b, 0x7b, 0x22, 0x66, 0x69, 0x72, 0x73, 0x74, 0x6e, 0x61, 0x6d, 0x65, 0x22, 0x3a, 0x22, 0x46, 0x72, 0x61, 0x6e, 0xc3, 0xa7, 0x6f, 0x69, 0x73, 0x22, 0x7d, 0x5d}
-	//fmt.Printf("test1: %#v\n", string(tBytes))
 }
-
-/*
-func toByte(s string) []byte {
-	return []byte(s)
-}
-*/
